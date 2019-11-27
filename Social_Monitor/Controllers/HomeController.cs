8a28fa07273c057ec676b9bdecfe4c0673b4cc31
Dinesh Rajendran <dineshdinesh052@gmail.com>
2019-11-27 23:07:40 +0530

    initial commit

---
+++ Social_Monitor/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Social_Monitor.Models;
+
+namespace Social_Monitor.Controllers
+{
+    public class HomeController : Controller
+    {
+        SearchEngine _searchEngine = new SearchEngine();
+        // GET: Home
+        public ActionResult Index()
+        {
+            dynamic jsonData = _searchEngine.CustomeSearch("Maggi");
+            var searchResult = new List<SearchResult>();
+            searchResult = _searchEngine.GetSearchResults(jsonData);
+            return View(searchResult.ToList());
+        }
+    }
+}
---
+++ Social_Monitor/Models/SearchEngine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Social_Monitor.Models
+{
+    public class SearchEngine
+    {
+        public string APIKEY = ConfigurationManager.AppSettings["APIKEY"];
+        public string EngineID = ConfigurationManager.AppSettings["EngineID"];
+        public string SearchURL = ConfigurationManager.AppSettings["SearchURL"];
+
+        public dynamic CustomeSearch(string searchString)
+        {
+            dynamic jsonData = null;
+            try
+            {
+                var request = WebRequest.Create(SearchURL + APIKEY + "&cx=" + EngineID + "&q=" + searchString);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string respString = reader.ReadToEnd();
+                jsonData = JsonConvert.DeserializeObject(respString);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return jsonData;
+        }
+
+        public List<SearchResult> GetSearchResults(dynamic jsonData)
+        {
+            var searchresult = new List<SearchResult>();
+            foreach(var item in jsonData.items)
+            {
+                searchresult.Add(new SearchResult
+                {
+                    Title = item.title,
+                    Link = item.link,
+                    Snippet = item.snippet,
+                    Source = item.source,
+                    Query = item.query,
+                    Index = item.index
+                    
+                });
+            }
+            return searchresult;
+        }
+    }
+}
